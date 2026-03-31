51e3999e94764a1c1748464629ec4520e148d67e
Martin <martin.duris@saturnia.sk>
2026-03-31 05:30:55 +0200

    rewritten to modern JS

--- MMM-MyWastePickup.js
+++ MMM-MyWastePickup.js
@@ -1,63 +1,60 @@
-Module.register('MMM-MyWastePickup', {
-
+Module.register("MMM-MyWastePickup", {
   defaults: {
     weeksToDisplay: 2,
     limitTo: 99,
     dateFormat: "D. MMM"
   },
 
-  // Define required styles.
-  getStyles: function () {
+  getStyles() {
     return ["MMM-MyWastePickup.css"];
-  },  
+  },
 
-  start: function() {
-    Log.info('Starting module: ' + this.name);
+  start() {
+    Log.info(`Starting module: ${this.name}`);
 
     this.nextPickups = [];
     this.initializing = true;
-
-    this.getPickups();
-
     this.timer = null;
 
+    this.getPickups();
   },
 
-  getPickups: function() {
-
+  getPickups() {
     clearTimeout(this.timer);
     this.timer = null;
 
-    this.sendSocketNotification("MMM-MYWASTEPICKUP-GET", {weeksToDisplay: this.config.weeksToDisplay, instanceId: this.identifier});
+    this.sendSocketNotification("MMM-MYWASTEPICKUP-GET", {
+      weeksToDisplay: this.config.weeksToDisplay,
+      instanceId: this.identifier
+    });
 
-    //set alarm to check again tomorrow
-    var self = this;
-    this.timer = setTimeout( function() {
-      self.getPickups();
-    }, 60 * 60 * 1000); //update once an hour
+    this.timer = setTimeout(() => {
+      this.getPickups();
+    }, 60 * 60 * 1000);
   },
 
-  socketNotificationReceived: function(notification, payload) {
-    if (notification == "MMM-MYWASTEPICKUP-RESPONSE" + this.identifier) {
+  socketNotificationReceived(notification, payload) {
+    if (notification === `MMM-MYWASTEPICKUP-RESPONSE${this.identifier}`) {
       this.nextPickups = payload;
       this.updateDom(1000);
     }
   },
 
-  svgIconFactory: function(glyph) {
+  svgIconFactory(glyph) {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttributeNS(null, "class", `waste-pickup-icon ${glyph}`);
 
-    var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
-    svg.setAttributeNS(null, "class", "waste-pickup-icon " + glyph);
-    var use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-    use.setAttributeNS("http://www.w3.org/1999/xlink", "href", this.file("icon_sprite.svg#") + glyph);
+    const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+    use.setAttributeNS("http://www.w3.org/1999/xlink", "href", `${this.file("icon_sprite.svg#")}${glyph}`);
+
     svg.appendChild(use);
-    
-    return(svg);
-  },   
 
-  getRelativeLabel: function(language, key, fallback) {
-    var localeData = moment.localeData(language);
-    var calendarFormat = localeData && localeData._calendar ? localeData._calendar[key] : null;
+    return svg;
+  },
+
+  getRelativeLabel(language, key, fallback) {
+    const localeData = moment.localeData(language);
+    const calendarFormat = localeData && localeData._calendar ? localeData._calendar[key] : null;
 
     if (!calendarFormat || typeof calendarFormat !== "string") {
       return fallback;
@@ -66,72 +63,67 @@ Module.register('MMM-MyWastePickup', {
     return calendarFormat
       .replace(/\[|\]/g, "")
       .replace(/LTS?/g, "")
-      .replace(/\s+(at|o)$/i, "")
       .replace(/\s+/g, " ")
+      .replace(/\s+(at|o)\s*$/i, "")
       .trim();
   },
 
-  getDom: function() {
-    var wrapper = document.createElement("div");
-    var language = ((this.config.language || config.language || "en") + "").toLowerCase();
+  getPickupDateLabel(pickupDate, language) {
+    const today = moment().startOf("day");
+    const tomorrow = moment(today).add(1, "days");
+    const nextWeek = moment(today).add(7, "days");
+    const localizedPickupDate = moment(pickupDate).locale(language);
 
-    if (this.initializing == true) {
+    if (today.isSame(localizedPickupDate)) {
+      return this.getRelativeLabel(language, "sameDay", "today");
+    }
+
+    if (tomorrow.isSame(localizedPickupDate)) {
+      return this.getRelativeLabel(language, "nextDay", "tomorrow");
+    }
+
+    if (nextWeek.isAfter(localizedPickupDate)) {
+      return localizedPickupDate.format("dddd");
+    }
+
+    return localizedPickupDate.format(this.config.dateFormat);
+  },
+
+  getDom() {
+    const wrapper = document.createElement("div");
+    const language = String(this.config.language || config.language || "en").toLowerCase();
+
+    if (this.initializing === true) {
       wrapper.innerHTML = this.translate("LOADING");
       wrapper.className = "dimmed light small";
       this.initializing = false;
       return wrapper;
     }
 
-    if(this.nextPickups.length == 0) {
+    if (this.nextPickups.length === 0) {
       console.error("[MMM-MyWastePickup] ERROR: No Schedule.");
       wrapper.innerHTML = "No schedule";
       wrapper.className = "light small";
       return wrapper;
     }
 
-    // this.nextPickups.forEach( function(pickup) {
-    for (var i = 0; i < this.nextPickups.length; i++) {
-
-      if (i == this.config.limitTo) {
-        break;
-      }
-
-      var pickup = this.nextPickups[i];
-
-      var pickupContainer = document.createElement("div");
+    for (const pickup of this.nextPickups.slice(0, this.config.limitTo)) {
+      const pickupContainer = document.createElement("div");
       pickupContainer.classList.add("pickup-container");
 
-      //add pickup date
-      var dateContainer = document.createElement("span");
+      const dateContainer = document.createElement("span");
       dateContainer.classList.add("pickup-date");
-
-      //determine how close pickup day is and formats accordingly.
-      var today = moment().startOf("day");
-      var pickUpDate = moment(pickup.pickupDate).locale(language);
-      if (today.isSame(pickUpDate)) {
-        dateContainer.innerHTML = this.getRelativeLabel(language, "sameDay", "today");
-      } else if (moment(today).add(1, "days").isSame(pickUpDate)) {
-        dateContainer.innerHTML = this.getRelativeLabel(language, "nextDay", "tomorrow");
-      } else if (moment(today).add(7, "days").isAfter(pickUpDate)) {
-        dateContainer.innerHTML = pickUpDate.format("dddd");
-      } else {
-        dateContainer.innerHTML = pickUpDate.format(this.config.dateFormat);
-      }
-
+      dateContainer.innerHTML = this.getPickupDateLabel(pickup.pickupDate, language);
       pickupContainer.appendChild(dateContainer);
 
-      //add waste type
-      var typeContainer = document.createElement("span");
+      const typeContainer = document.createElement("span");
       typeContainer.classList.add("waste-pickup-type");
       typeContainer.innerHTML = pickup.Type;
-
       pickupContainer.appendChild(typeContainer);
 
       wrapper.appendChild(pickupContainer);
-
     }
 
     return wrapper;
   }
-
 });
